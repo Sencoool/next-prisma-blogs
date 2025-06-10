b9b0d797b42f4f85789495b7a3836c415d5de5d2
Sencoool <s6506021610037@email.kmutnb.ac.th>
2025-06-10 23:56:59 +0700

    Get unique route create

---
+++ app/api/post/[id]/route.ts
@@ -0,0 +1,18 @@
+import Posts from "@/app/post/page";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const postId = Number(params.id);
+  const Post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+  });
+
+  return Response.json(Post);
+}
