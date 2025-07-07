c596adb5b7f0ac752955515b192848578254f39a
Sencoool <s6506021610037@email.kmutnb.ac.th>
2025-07-07 14:17:09 +0700

    Package: adding postinstall script
    Fix: valid type for Vercel deployment
    
    Fix: PrismaClient importing

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: number } }
 ) {
   const { id } = await params; // Next 15 must await dynamic API to access its properties
   const postId = Number(id);
