89a9441235816a29205eb3a98599c13b1153f7e0
Sencoool <s6506021610037@email.kmutnb.ac.th>
2025-07-07 14:51:30 +0700

    Vercel: Test deployment

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -2,53 +2,53 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: number } }
-) {
-  const { id } = await params; // Next 15 must await dynamic API to access its properties
-  const postId = Number(id);
-  const Post = await prisma.post.findUnique({
-    where: {
-      id: postId,
-    },
-  });
+// export async function GET(
+//   request: Request,
+//   { params }: { params: { id: number } }
+// ) {
+//   const { id } = await params; // Next 15 must await dynamic API to access its properties
+//   const postId = Number(id);
+//   const Post = await prisma.post.findUnique({
+//     where: {
+//       id: postId,
+//     },
+//   });
 
-  return Response.json(Post);
-}
+//   return Response.json(Post);
+// }
 
-export async function PUT(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const { title, content, published, authorId } = await request.json();
-  const { id } = await params;
-  const postId = Number(id);
-  const Post = await prisma.post.update({
-    where: {
-      id: postId,
-    },
-    data: {
-      title,
-      content,
-      published,
-      authorId,
-    },
-  });
+// export async function PUT(
+//   request: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const { title, content, published, authorId } = await request.json();
+//   const { id } = await params;
+//   const postId = Number(id);
+//   const Post = await prisma.post.update({
+//     where: {
+//       id: postId,
+//     },
+//     data: {
+//       title,
+//       content,
+//       published,
+//       authorId,
+//     },
+//   });
 
-  return Response.json(Post);
-}
+//   return Response.json(Post);
+// }
 
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const { id } = await params;
-  const postId = Number(id);
-  const Post = await prisma.post.delete({
-    where: {
-      id: postId,
-    },
-  });
-  return Response.json({ message: `Delete post id ${postId} complete` });
-}
+// export async function DELETE(
+//   request: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const { id } = await params;
+//   const postId = Number(id);
+//   const Post = await prisma.post.delete({
+//     where: {
+//       id: postId,
+//     },
+//   });
+//   return Response.json({ message: `Delete post id ${postId} complete` });
+// }
